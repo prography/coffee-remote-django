801a926a5f5d76db8eb6a4afa45448e99c3985d6
suev7245 <suev7245@gmail.com>
2019-04-16 11:01:49 +0900

    chore: add comments

--- coffee/settings.py
+++ coffee/settings.py
@@ -7,6 +7,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
 
+
+# 공통부분, 개발모드/배포모드 로 나누어 파일 참조함. 위의 DEBUG=True값만 조정해주면 됨.
 SECRET_DIR = os.path.join(BASE_DIR, '.config_secret')
 SECRET_COMMON_FILE = os.path.join(SECRET_DIR, 'common.json')
 if DEBUG==True:
@@ -20,8 +22,8 @@ secret_other = json.loads(open(SECRET_FILE).read())
 SECRET_KEY = secret_common['django']["secret_key"]
 ALLOWED_HOSTS = secret_other['django']['allowed_hosts']
 
-# Application definition
 
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
