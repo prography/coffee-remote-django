f13d4ea2a66abfa31f939a73704fd1038b5da7f9
suev7245 <suev7245@gmail.com>
2019-04-20 17:50:20 +0900

    fix: change id_field int to char

--- order/models.py
+++ order/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Order(models.Model):
-    unique_id = models.IntegerField(default=0)
+    unique_id = models.CharField(max_length=30, default=0)
     created_at = models.DateTimeField(default=timezone.now)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     pickuptime = models.IntegerField()
