68a5a6c439160962e3326469c1850f5b601145ac
suev7245 <suev7245@gmail.com>
2019-07-08 23:01:35 +0900

    seperate kakaoAPI with order function

--- coffee/urls.py
+++ coffee/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from menu import views as menu_views
 from order import views as order_views
+from kakao import views as kakao_views
 from rest_auth.views import LogoutView
 
 
@@ -12,11 +13,11 @@ router.register(r'menu', menu_views.MenuViewSet)
 router.register(r'order', order_views.OrderViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rest-auth/kakao/', order_views.KakaoLogin.as_view(), name='kakao_login'),
-    # path('accounts/kakao/login/', order_views.oauthCode),
-    # path('accounts/kakao/login/callback/', order_views.oauthCodeCallback),
-    # path('accounts/kakao/logout/', order_views.oauthLogOut),
-    # path('accounts/kakao/check/', order_views.oauthLogOut),
+    path('rest-auth/kakao/', kakao_views.KakaoLogin.as_view(), name='kakao_login'),
+    # path('accounts/kakao/login/', kakao_views.oauthCode),
+    # path('accounts/kakao/login/callback/', kakao_views.oauthCodeCallback),
+    # path('accounts/kakao/logout/', kakao_views.oauthLogOut),
+    # path('accounts/kakao/check/', kakao_views.oauthLogOut),
     path('', include(router.urls)),
     path('logout/', LogoutView.as_view(), name='rest_logout'),
 ]
